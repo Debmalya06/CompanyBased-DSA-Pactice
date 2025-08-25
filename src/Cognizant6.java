2ecca2e39066df80021064afa4e0fae15464cb98
Deb006P <debmalyapan4@gmail.com>
2025-08-26 02:22:16 +0530

    cognizant6

--- src/Cognizant6.java
+++ src/Cognizant6.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Cognizant6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the month:");
+        int month = sc.nextInt();
+       
+        if(month<1 || month>12){
+            System.out.println("Invaild Month!!");
+            return;
+        } else if (month>=3 && month<=5) {
+            System.out.println("Spring");
+        } else if (month>=5 && month<=8) {
+            System.out.println("Summer");
+        } else if (month>=9 && month<=11 ) {
+            System.out.println("Autumn");
+        }
+        else {
+            System.out.println("Winter");
+        }
+
+    }
 }
