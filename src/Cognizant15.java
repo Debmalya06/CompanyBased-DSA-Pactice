ffadf0efafe5d34c06e815e13674b5db5dd5b5b0
Deb006P <debmalyapan4@gmail.com>
2025-08-30 12:20:49 +0530

    change file name

--- src/Cognizant15.java
+++ src/Cognizant15.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Capgemini15 {
+public class Cognizant15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
