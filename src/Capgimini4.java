c48efc30c98fd17c80f065e8e354ae2e2badacfe
Deb006P <debmalyapan4@gmail.com>
2025-09-17 11:53:04 +0530

    add new code

---
+++ src/Capgimini4.java
@@ -0,0 +1,9 @@
+public class Capgimini4 {
+    public  static  void solveEquation(int a, int b){
+        int c = ((a*3)+(a*(2*b))+((2*a)*(2*b))+((2*a*b)*2)+(a*b*2)+(b*3));
+        System.out.println(c);
+    }
+    public static void main(String args[]){
+        solveEquation(2,3);
+    }
+}
