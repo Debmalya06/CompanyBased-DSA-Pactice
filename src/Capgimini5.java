4500f11495c2f89464cbba776b77dcfc6b3b9223
Deb006P <debmalyapan4@gmail.com>
2025-09-17 12:40:04 +0530

    add new code

---
+++ src/Capgimini5.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Capgimini5 {
+    public static void main(String[] args) {
+        String s ="##Move##Hash#To#Front";
+        StringBuilder sb = new StringBuilder(s);
+        for(int i=sb.length()-1;i>=0;i--){
+            if (sb.charAt(i) == '#') {
+                sb.deleteCharAt(i);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
