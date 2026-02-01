3a43afebd079624863363ae531c079ff834c1ac7
Deb006P <debmalyapan4@gmail.com>
2026-02-01 21:23:22 +0530

    add questions on new deloitte code.

--- src/deloitte4.java
+++ src/deloitte4.java
@@ -1,3 +1,10 @@
+//Input n 4
+//m:20
+//Output 90
+//Explanation
+//Sum of numbers divisible by 4 are 4+ 8+ 12+ 16 20=60
+//Sum of numbers not divisible by 4 are 1+2+ 3+5+ 6+ 7+9+10+ 11+13+ 14 +15+17+18+ 19 = 150
+// Difference 150-60 =90
 import java.util.Scanner;
 
 public class deloitte4 {
@@ -8,6 +15,7 @@ public class deloitte4 {
             if(i%n==0) evensum+=i;
             else oddsum+=i;
         }
-        System.out.println(oddsum + " "+evensum);
+
+        System.out.println(oddsum - evensum);
     }
 }
