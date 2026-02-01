0b4521d883027b9f9bb24ed958e1765bc868e7da
Deb006P <debmalyapan4@gmail.com>
2026-02-01 22:58:52 +0530

    add new questions and ans(code)

---
+++ src/deloitte6.java
@@ -0,0 +1,21 @@
+//palimdrome
+
+import java.util.*;
+
+public class deloitte6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int org=n;
+        int rev=0;
+        while(n!=0){
+            int x=n%10;
+             rev=rev*10+x;
+            n=n/10;
+        }
+        if(rev==org) System.out.println("palimdrome");
+        else System.out.println("not palimdrome");
+    }
+
+
+}
